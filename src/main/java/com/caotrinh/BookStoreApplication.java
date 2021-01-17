08b8df2714eb0f23dfb1b501665a54a7515f2a8b
hoangvietnd <55285606+hoangvietnd@users.noreply.github.com>
2021-01-17 18:27:35 +0700

    deploy to Tomcat server

--- src/main/java/com/caotrinh/BookStoreApplication.java
+++ src/main/java/com/caotrinh/BookStoreApplication.java
@@ -2,9 +2,16 @@ package com.caotrinh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookStoreApplication {
+public class BookStoreApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookStoreApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
